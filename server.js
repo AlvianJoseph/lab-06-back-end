02bdf2fd9643505eda46591f5a3876c63f6eec22
Alvian Joseph <alvianjoseph@gmail.com>
2019-05-14 12:54:56 -0700

    initial weather app complete

--- server.js
+++ server.js
@@ -32,4 +32,36 @@ function Location(query, geoData) {
     this.longitude = geoData.results[0].geometry.location.lng;
 }
 
+// const forecastArr = [];
+// // app.get('data/darksky.json')
+// //         .then(weatherObjects => {
+// //             weatherObjects.forEach(forecast => forecastArr.push(new Location(query, geoData)));
+// //     })
+
+app.get('/weather', (request, response) => {
+    try {
+        const weatherData = searchForecast(request.query.data);
+        response.send(weatherData);
+
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send('Status: 500. Sorry, something went wrong');
+    }
+});
+
+function searchForecast (query) {
+    const weatherData = require('./data/darksky.json');
+    // weatherData.forEach( () => forecastArr.push(new Location(query, weatherData)))
+    // request.send(forecastArr)
+    const forecast = new Forecast(query, weatherData)
+    return forecast;
+}
+
+function Forecast(query, weatherData) {
+    this.search_query = query;
+    this.Forecast = weatherData.hourly.summary;
+    this.Time = weatherData.hourly.data[0].time
+}
+
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
