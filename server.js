ed52dff97cc691f47d7607920f788d166d1e6711
richard0923 <ricky16rosado@gmail.com>
2019-05-14 13:21:37 -0700

    done for today

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const PORT = process.env.PORT;
+const PORT = herokuapp.com;
 const app = express();
 app.use(cors());
 
@@ -61,7 +61,7 @@ function searchForecast (query) {
 function Forecast(query, weatherData) {
     this.search_query = query;
     this.Forecast = weatherData.hourly.summary;
-    this.Time = new Date(weatherData.hourly.data[0].time);
+    this.Time = newweatherData.hourly.data[0].time;
 }
 
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
