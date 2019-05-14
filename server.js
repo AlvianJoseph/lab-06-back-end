bda12b908ed18fa15c016afe1fe4fd353d9d4321
Alvian Joseph <alvianjoseph@gmail.com>
2019-05-14 14:15:10 -0700

    added public port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 
